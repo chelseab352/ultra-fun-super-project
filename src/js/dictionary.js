0ea2011ba827afcf15151cb6759eda6b9e538c17
Tim Pratt <tpratt@352media.com>
2018-07-13 13:25:13 -0400

    adding dictionary

--- src/js/dictionary.js
+++ src/js/dictionary.js
@@ -1,3 +1,156 @@
 module.exports = {
 
+adverbs: [
+],
+nouns: [
+],
+activity-present: [],
+
+activity-past: [
+'danced',
+'farted',
+'sang',
+'exercised',
+'stripped',
+'fought',
+'slept',
+'drank',
+'painted',
+'drove'
+],
+
+place:[
+'mall',
+'hospital',
+'museum',
+'playground',
+'I-Hop',
+'airport terminal',
+'open field of wheat',
+'mountain top',
+'public fountain',
+'flea market',
+'truck stop',
+'library',
+'lamp store',
+'wax museum',
+'log cabin',
+'roadside circus'
+],
+
+animals: [
+'confused penguins',
+'innebriated bears',
+'drunk flamingos',
+'tangled snakes',
+'fruit bats',
+'angry racoons',
+'silly salamanders',
+'depressed wallabies'
+],
+
+song: [],
+
+food: [],
+
+job: [
+'pig greaser',
+'cow farrier',
+'candlestick melter',
+'deoderant tester',
+'mousetrap inspector',
+'craft bleach maker',
+'noodle inspector',
+'silent movie critic',
+'potato polisher',
+'industrial chemical taster',
+'combat chicken trainer',
+'dolphin breeder'
+
+],
+
+job-plural: [
+'pig greasers',
+'cow farriers',
+'candlestick melters',
+'deoderant testers',
+'mousetrap inspectors',
+'craft bleach makers',
+'noodle inspectors',
+'silent movie critics',
+'potato polishers',
+'industrial chemical tasters',
+'combat chicken trainers',
+'dolphin breeders'
+],
+
+country: [
+'Latvia',
+'Bulgaria',
+'Kazakhstan',
+'Albania',
+'Liechtenstein',
+'Slovenia',
+],
+
+business: [],
+
+year: [],
+
+hobbies: [
+'competitive crocheting',
+'pickle collecting',
+'cake sniffing',
+'non-interpretive dancing',
+'silent singing',
+'fish-sauce tasting',
+'collecting dead animals',
+'painting pinecones',
+'collecting vintage pepper shakers',
+'ripping the white tags off of matresses',
+'painting night time landscapes at night',
+'reading old phone books',
+'rubber band achery',
+'backwards jogging',
+'snail racing',
+'crafting birdhouses without entrances',
+'competitive nude spelunking',
+'underwater basket weaving',
+'STD collecting',
+'household chemical tasting',
+'underground squirrel fighting',
+'artistic black and white navel photography',
+'competitive soap making'
+],
+
+sport: [
+'uphill bobsledding',
+'400 meter potato sack racing',
+'blindfolded archery',
+'mental gymnastics',
+'pole dancing',
+'hatched egg relay',
+'lion mane braiding',
+'greased pig wrestling',
+'race walking',
+'full contact badminton',
+'skateless speed skating',
+'1 meter sprinting'
+],
+
+funfacts: [
+'Once (activity-past) in a (place) with a group of (animals) while singing (song).',
+'Was arrested for illegal (activity) in the nude while eating (food).',
+'Parents were (job-plural) in the country of (country) before immigrating to the United States.',
+'Worked as a (job) for 10 years at an illegal (business) before joining 352.',
+'Was a semi-profesional (activity) at a nudist colony in (country).',
+'Once (activity) a man in Reno, just to watch him (activity).',
+'Competed in the (year) Unofficial Olympics in the sport of (sport), but was disqualified for excessive (activity).',
+'Hobbies include (hobbies), (hobbies), and (hobbies).',
+'Likes to (activity) in the spring time in (country).',
+''
+]
+
+
+}
 };
