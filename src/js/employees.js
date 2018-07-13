df07a6dd96262c9680e6127820a2a58abaf5faca
bprymicz <bradprymicz@gmail.com>
2018-07-13 13:05:09 -0400

    more photos and data

--- src/js/employees.js
+++ src/js/employees.js
@@ -1,60 +1,434 @@
-
-
-[
+module.exports = [
     {
-        name: 'Lindsay Clifton',
-        position: 'Administrator',
-        team: 'Admin',
-        gender: 'F',
-        photo: 'Administrative-Lindsay-Clifton.jpg'
+        name:"Magbic Aleman",
+        position:"Software Developer",
+        team:"Pizza Party (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-Circle-Magbic-Aleman.jpg",
+        gender:"M"
     },
     {
-        name: 'Eric Cunningham',
-        position: 'DevOps',
-        team: 'DevOps',
-        gender: 'M',
-        photo: 'DevOps-Eric-Cunningham.jpg'
+        name:"Lincoln Anderson",
+        position:"Associate Design Director",
+        team:"Red (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Design - Visual Design",
+        photo:"Team-Red-Lincoln-Anderson.jpg",
+        gender:"M"
     },
     {
-        name: 'Kierra Green',
-        position: 'Sales',
-        team: 'Mermaid',
-        gender: 'F',
-        photo: 'Kierra-Greene.jpg'
+        name:"Sarah Barnett",
+        position:"Director of Client Engagement",
+        team:"Sales",
+        location:"ATLANTA, GA OFFICE",
+        department:"Sales",
+        photo:"",
+        gender:"F"
     },
     {
-        name: 'Megan Hord',
-        position: 'Content Developer',
-        team: 'Product Growth',
-        gender: 'F',
-        photo: 'Product-Growth-_-Megan-Hord.jpg'
+        name:"Daniel Barreto",
+        position:"Scrum Master",
+        team:"Diesel (dev) / Blue (SLA) / Covering for Pizza Party (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Scrum Masters",
+        photo:"danny-barreto.jpg",
+        gender:"F"
     },
     {
-        name: 'Brian Russell',
-        position: 'Senior Marketing Strategist',
-        team: 'Product Growth',
-        gender: 'M',
-        photo: 'Product-Growth-Brian-Russell.jpg'
+        name:"Robert Berris",
+        position:"VP, Digital Strategy",
+        team:"Leads Innovation department and designers",
+        location:"ATLANTA, GA OFFICE",
+        department:"Leadership & Partners",
+        photo:"berris-750x938.jpg",
+        gender:"M"
     },
     {
-        name: 'Chelsea Burns',
-        position: 'Boss Bitch',
-        team: 'Product Growth',
-        gender: 'F',
-        photo: 'Product-Growth-Chelsea-Burns.jpg'
+        name:"Caroline Blake",
+        position:"Partner",
+        team:"Handles company financials and benefits / Covering project scheduling",
+        location:"TAMPA, FL OFFICE",
+        department:"Leadership & Partners",
+        photo:"caroline-720x720-375x375.jpg",
+        gender:"F"
     },
     {
-        name: 'Damion Wasylow',
-        position: 'Senior Marketing Strategist',
-        team: 'Product Growth',
-        gender: 'M',
-        photo: 'Product-Growth-Damion-Wasylow.jpg'
+        name:"Richard Brookfield",
+        position:"Director of Technology",
+        team:"Apollo (352 Internal Product team) / Sales estimation",
+        location:"TAMPA, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-I-4-Rich-Brookfield.jpg",
+        gender:"M"
     },
     {
-        name: 'Erin Devine Martin',
-        position: 'Innovationist',
-        team: 'Innovation',
-        gender: 'F',
-        photo: 'Product-Growth-Erin-Devine-Martin.jpg'
+        name:"Michelle Brownstein",
+        position:"Interactive Designer",
+        team:"Red (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Design - Front End Dev",
+        photo:"Team-UX-Michelle-Brownstein.jpg",
+        gender:"F"
     },
-]
+    {
+        name:"Peter Brownstein",
+        position:"Software Developer",
+        team:"Red (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-Red-Peter-Brownstein.jpg",
+        gender:"M"
+    },
+    {
+        name:"Chelsea Burns",
+        position:"Senior Marketing Strategist",
+        team:"SLCE (Product Growth)",
+        location:"TAMPA, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-Chelsea-Burns.jpg",
+        gender:"F"
+    },
+    {
+        name:"Christopher Burns",
+        position:"Scrum Master",
+        team:"Apollo (352 Internal Product team)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Product Growth - 352 Products",
+        photo:"Team-Red-Chris-Burns.jpg",
+        gender:"M"
+    },
+    {
+        name:"Kayla Burrell",
+        position:"ScrumMaster",
+        team:"Pizza Party (dev) / Blue (SLA)",
+        location:"TAMPA, FL OFFICE",
+        department:"Scrum Masters",
+        photo:"Team-Popo-Kayla-Burrell.jpg",
+        gender:"F"
+    },
+    {
+        name:"Lori Chiappe",
+        position:"VP of Product Growth",
+        team:"Leads Product Growth ",
+        location:"ATLANTA, GA OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"lori.jpg",
+        gender:"F"
+    },
+    {
+        name:"Lindsay Clifton",
+        position:"Executive Assistant",
+        team:"Coordinates Innovation work / Admin Asst / Atlanta office manager",
+        location:"ATLANTA, GA OFFICE",
+        department:"Administrative",
+        photo:"Administrative-Lindsay-Clifton.jpg",
+        gender:"F"
+    },
+    {
+        name:"Eric Cunningham",
+        position:"DevOps Engineer",
+        team:"DevOps",
+        location:"NEWBERRY, FL OFFICE",
+        department:"DevOps",
+        photo:"DevOps-Eric-Cunningham.jpg",
+        gender:"M"
+    },
+    {
+        name:"Victor Davis",
+        position:"Quality Assurance Analyst",
+        team:"Pizza Party (dev) / Diesel (dev) / Blue (SLA)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"QA",
+        photo:"Team-Fusion-Victor-Davis.jpg",
+        gender:"M"
+    },
+    {
+        name:"Edward Egun",
+        position:"Software Developer",
+        team:"Blue (SLA)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-Blue-Ed-Egun.jpg",
+        gender:"M"
+    },
+    {
+        name:"Scott Feigelis",
+        position:"Interactive Designer",
+        team:"Pizza Party (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Design - Visual Design",
+        photo:"Team-Circle-Scott-Fiegelis.jpg",
+        gender:"M"
+    },
+    {
+        name:"Nicholas Forneris",
+        position:"Interactive Designer",
+        team:"Orange (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Design - Visual Design",
+        photo:"Team-Orange-Nick-Forneris.jpg",
+        gender:"M"
+    },
+    {
+        name:"Mardy Freeman",
+        position:"Office Manager",
+        team:"Gainesville office manager",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Administrative",
+        photo:"andi-freeman.jpg",
+        gender:"F"
+    },
+    {
+        name:"Christa Garcia",
+        position:"VP of Operations",
+        team:"Leads Scrum Masters and oversees dev teams",
+        location:"TAMPA, FL OFFICE",
+        department:"Leadership & Partners",
+        photo:"Leadership-Christa-Garcia-375x375.jpg",
+        gender:"F"
+    },
+    {
+        name:"Richard Goidel",
+        position:"Director of Innovation",
+        team:"Innovation",
+        location:"ATLANTA, GA OFFICE",
+        department:"Innovation",
+        photo:"Rich-Goidel.jpg",
+        gender:"M"
+    },
+    {
+        name:"Kacie Gordon",
+        position:"VP, Client Engagement",
+        team:"Sales",
+        location:"ATLANTA, GA OFFICE",
+        department:"Sales",
+        photo:"Kacie_Lett-375x375.jpg",
+        gender:"F"
+    },
+    {
+        name:"Kierra Greene",
+        position:"Sales Manager",
+        team:"Sales",
+        location:"ATLANTA, GA OFFICE",
+        department:"Sales",
+        photo:"Kierra-Greene.jpg",
+        gender:"F"
+    },
+    {
+        name:"Megan Hord",
+        position:"Content Marketing Strategist",
+        team:"PG Specialist",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-_-Megan-Hord.jpg",
+        gender:"F"
+    },
+    {
+        name:"Kevin James",
+        position:"Interactive Designer",
+        team:"Diesel (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-Kevin-James.jpg",
+        gender:"M"
+    },
+    {
+        name:"Ashish Johri",
+        position:"Quality Assurance Analyst",
+        team:"Orange (dev) / Red (dev) / Blue (SLA)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"QA",
+        photo:"Team-QA-Ashish-Johri.jpg",
+        gender:"M"
+    },
+    {
+        name:"Peter Juchniewicz",
+        position:"Software Developer",
+        team:"Orange (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-Orange-Peter-Juchniewicz.jpg",
+        gender:"M"
+    },
+    {
+        name:"Lauren LeDonne",
+        position:"Senior Marketing Strategist",
+        team:"SLCE (Product Growth)",
+        location:"TAMPA, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-Lauren-LeDonne.jpg",
+        gender:"F"
+    },
+    {
+        name:"Sophie Loghman",
+        position:"Product Marketing Strategiest",
+        team:"SLCE (Product Growth)",
+        location:"ATLANTA, GA OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-Sophie-Loghman.jpg",
+        gender:"F"
+    },
+    {
+        name:"Erin Martin",
+        position:"Marketing Strategist",
+        team:"Apollo (352 Internal Product team)",
+        location:"ATLANTA, GA OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-Erin-Devine-Martin.jpg",
+        gender:"F"
+    },
+    {
+        name:"Andrea Mausser",
+        position:"Product Marketing Strategist",
+        team:"SLCE (Product Growth)",
+        location:"TAMPA, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"andrea-mausser.jpg",
+        gender:"F"
+    },
+    {
+        name:"Blake McLeod",
+        position:"Front-End Developer",
+        team:"Orange (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Design - Front End Dev",
+        photo:"Team-Orange-Blake-McLeod.jpg",
+        gender:"M"
+    },
+    {
+        name:"Andrew Mimault",
+        position:"Digital Media Strategist",
+        team:"PG Specialist",
+        location:"TAMPA, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"andrew-mimault.jpg",
+        gender:"M"
+    },
+    {
+        name:"Brandon Mitchell",
+        position:"Interactive Designer",
+        team:"Diesel (dev)",
+        location:"WFH - FLORIDA WINTER PARK, FL",
+        department:"Design - Front End Dev",
+        photo:"Team-I-4-Brandon-Mitchell.jpg",
+        gender:"M"
+    },
+    {
+        name:"Jeremy New",
+        position:"Analytics Manager",
+        team:"PG Specialist",
+        location:"TAMPA, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"jeremy-new.jpg",
+        gender:"M"
+    },
+    {
+        name:"Jonathan Pitts",
+        position:"Software Developer",
+        team:"Diesel (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Development",
+        photo:"Team_Blue_Jon_Pitts-1.jpg",
+        gender:"M"
+    },
+    {
+        name:"Timothy Pratt",
+        position:"Interactive Designer",
+        team:"PG Specialist",
+        location:"WFH - FLORIDA ORMOND BEACH, FL",
+        department:"Design - Visual Design",
+        photo:"Team-I-4-Tim-Pratt.jpg",
+        gender:"M"
+    },
+    {
+        name:"Bradley Prymicz",
+        position:"Software Developer",
+        team:"Blue (SLA)",
+        location:"TAMPA, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-I-4-Brad-Prymicz-1.jpg",
+        gender:"M"
+    },
+    {
+        name:"Brian Russell",
+        position:"Senior Marketing Strategist",
+        team:"PG Specialist",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-Brian-Russell.jpg",
+        gender:"M"
+    },
+    {
+        name:"Logan Sobczak",
+        position:"Software Developer",
+        team:"Blue (SLA)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Developer",
+        photo:"Product-Growth-Brian-Russell.jpg",
+        gender:"M"
+    },
+    {
+        name:"Eran Schoellhorn",
+        position:"Director of Technology",
+        team:"Leads developers and QA",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-Circle-Eran-Schoellhorn.jpg",
+        gender:"M"
+    },
+    {
+        name:"Kristin Scoufis",
+        position:"Associate UX Director",
+        team:"Cross-company UX resource",
+        location:"TAMPA, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Team-UX-Krissy-Scoufis.jpg",
+        gender:"F"
+    },
+    {
+        name:"Michael Sheftic",
+        position:"Software Developer",
+        team:"Red (dev)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Software Development",
+        photo:"Team-Orange-Michael-Sheftic.jpg",
+        gender:"M"
+    },
+    {
+        name:"Paul Traylor",
+        position:"Interactive Designer",
+        team:"Blue (SLA)",
+        location:"WFH - BLACK MOUNTAIN, NC",
+        department:"Design - Front End Dev",
+        photo:"Team-Blue-Paul-Traylor.jpg",
+        gender:"M"
+    },
+    {
+        name:"Brent Walbolt",
+        position:"Interactive Designer",
+        team:"PG Specialist",
+        location:"TAMPA, FL OFFICE",
+        department:"Design - Visual Design",
+        photo:"Team-Popo-Brent-Walbolt.jpg",
+        gender:"M"
+    },
+    {
+        name:"Damion Wasylow",
+        position:"Senior Marketing Strategist",
+        team:"Silver (Product Growth)",
+        location:"NEWBERRY, FL OFFICE",
+        department:"Product Growth - Client Marketing",
+        photo:"Product-Growth-Damion-Wasylow.jpg",
+        gender:"M"
+    },
+    {
+        name:"Geoffrey Wilson",
+        position:"President & Founding Partner",
+        team:"Oversees company",
+        location:"ATLANTA, GA OFFICE",
+        department:"Leadership & Partners",
+        photo:"geoff.jpg",
+        gender:"M"
+    }
+];
